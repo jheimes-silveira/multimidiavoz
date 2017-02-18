40d738874bc2c4c8ff0c6c5224d6bfd3138d9966
yeebaplay <vinicius.parise@hotmail.com>
2017-02-18 15:48:24 -0200

    Criacao da tela de login e cadastro

---
+++ app/src/main/java/br/org/multimidia/multimidiavoz/ActCadastro.java
@@ -0,0 +1,13 @@
+package br.org.multimidia.multimidiavoz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_cadastro);
+    }
+}
---
+++ app/src/main/java/br/org/multimidia/multimidiavoz/ActLogin.java
@@ -0,0 +1,13 @@
+package br.org.multimidia.multimidiavoz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_login);
+    }
+}
